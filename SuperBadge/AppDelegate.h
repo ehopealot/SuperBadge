a98a7070c3f77cd340da48371e633ff2a676aedd
Erik Hope <erikjhope@gmail.com>
2012-04-19 21:18:51 -0500

    Initial Commit

---
+++ SuperBadge/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SuperBadge
+//
+//  Created by Erik Hope on 4/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SuperBadge/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SuperBadge
+//
+//  Created by Erik Hope on 4/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
